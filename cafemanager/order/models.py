93baa996b46cc9d116c824f3bbafdaddf9978ba2
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-01-24 15:28:08 +0300

    Add Order model

--- cafemanager/order/models.py
+++ cafemanager/order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    table_number = models.IntegerField(verbose_name='Номер стола')
+    order_id = models.CharField(max_length=10, unique=True, verbose_name='ID заказа')
+    items = models.TextField(verbose_name='Заказанные блюда')
+    status = models.CharField(max_length=20, default='в ожидании', verbose_name='Статус')
+
+    def __str__(self):
+        return f'Заказ № {self.order_id} на стол № {self.table_number}'
