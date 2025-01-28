8e1f89e55efb0b2f23f537b09b803479dcba77d4
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-01-28 11:10:10 +0300

    Create api app,  install drf, edit project settings

--- cafemanager/cafemanager/settings.py
+++ cafemanager/cafemanager/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'order.apps.OrderConfig',
+    'api.apps.ApiConfig',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
