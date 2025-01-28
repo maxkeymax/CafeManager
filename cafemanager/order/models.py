f1b3ef5ca9edf90483b782dc1d0ca109d7acf37d
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-01-28 09:21:33 +0300

    Edit status values

--- cafemanager/order/models.py
+++ cafemanager/order/models.py
@@ -10,7 +10,7 @@ class Order(models.Model):
     table_number = models.IntegerField(verbose_name='Номер стола')
     items = models.TextField(verbose_name='Заказанные блюда')
     total_price = models.IntegerField(verbose_name='Общая стоимость', default=0)
-    status = models.CharField(max_length=10, choices=STATUS_CHOISES, default='в ожидании', verbose_name='Статус')
+    status = models.CharField(max_length=10, choices=STATUS_CHOISES, default='В ожидании', verbose_name='Статус')
 
     def __str__(self):
         return f'Заказ № {self.id} на стол № {self.table_number} статус: {self.status}'
